31fa0bc913d4e4f2c594a40ada5d4e38a22dc84a
User <User@WIN-8L3JPGP323N>
2023-10-30 15:08:18 +0600

    Добавьте файлы проекта.

---
+++ Assignment6/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Assignment6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            {
+                string newItem = textBox1.Text;
+                if (!string.IsNullOrEmpty(newItem))
+                {
+                    listBox1.Items.Add(newItem);
+                    textBox1.Clear();
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            {
+                if (listBox1.SelectedIndex != -1)
+                {
+                    listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DialogResult r = MessageBox.Show("Are you sure?", "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+            if (r == DialogResult.Yes)
+            {
+                listBox1.Items.Clear();
+            }
+            if (r == DialogResult.No)
+            {
+                MessageBox.Show("ok");
+            }
+        }
+    }
+}
